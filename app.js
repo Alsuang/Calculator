29e1e2cdc1c72fbf0455a994eb851a7afb5085fa
Aldrin Tunque <atunque22@gmail.com>
2023-02-08 12:35:51 -0800

    Working Calculator

--- app.js
+++ app.js
@@ -1,34 +1,86 @@
-function sum (num1, num2){
 
- return num1 + num2 
+const sum = function (num1, num2){
+  return num1 + num2 
 }
-function subtract (num1, num2){
+const subtract = function (num1, num2){
   return num1 - num2 
 }
-function divide (num1, num2){
+const divide = function (num1, num2){
   return num1 / num2 
 }
-function multiply (num1, num2){
+const multiply = function (num1, num2){
   return num1 * num2 
 }
 
-function operate (operator, num1, num2){
-  return operator(num1, num2)
+function calculator(num1, num2, operate){
+  return operate(num1, num2)
 }
 
 
-let numString = ''
-function pressedDigit() {
+let numString1 = '';
+let numString2 = '';
+let operation = '';
 
-let userPressed = $(this).attr("value");
- numString += userPressed;
+function getNumber() {
+  let userDigit = $(this).attr("value");
+  if (operation !== ''){
+    numString2 += userDigit;
+    $('.display').attr("value", numString2);
+    numString2 = Number(numString2);
+    return numString2;
 
-  $('.display').attr("value", numString);
+  } else {
+  
+    numString1 += userDigit;
+    $('.display').attr("value", numString1);
+    numString1 = Number(numString1);
+    return numString1;
+  }
+  }
+
+
+function getOperator() {
+  let userOperator =$(this).attr("value");
+  operation += userOperator;
+  return operation;
+}
+
+
+function clearDisplay() {
+  $('.display').attr("value", '');
+  numString1 = '';
+  numString2 = '';
+  operation = '';
+}
  
- }
- $('.digit').on('click', pressedDigit);
- 
- 
+
+
+$('.equals').on('click', function() {
+  if (operation === 'multiply') {
+  $('.display').attr("value", multiply(numString1, numString2));
+  
+    }else if (operation === 'sum'){
+  $('.display').attr("value", sum(numString1, numString2))
+  
+    }else if (operation === 'divide'){
+      $('.display').attr("value", divide(numString1, numString2))
+    
+    }else {
+    $('.display').attr("value", subtract(numString1, numString2))
+    }
+  });
+
+
+
+
+$('.clear').on('click', clearDisplay);
+ $('.digit').on('click', getNumber);
+ $('.operator').on('click', getOperator);
+
+
+
+
+
  
  
  
